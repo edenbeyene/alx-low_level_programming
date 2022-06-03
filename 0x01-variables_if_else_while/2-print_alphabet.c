1448687a78b1e95d1f88b331f23e6fdf16e65f98
Your Name <you@example.com>
2022-06-02 23:08:49 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	
+	char l;
+      
+   
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+ 	putchar( '\n' );
+                	
+                    
+	return (0);
+}
