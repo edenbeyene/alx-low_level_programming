db98497b2b7be5a86d6384185eb4a4c738dea2fc
Your Name <you@example.com>
2022-06-03 00:30:46 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+int i;
+int  j;
+for (i='0';i<='9';i++)
+{
+	for(j=i;j<='9';j++)
+	{
+		if(i!=j){
+		 putchar(i);
+		  putchar(j);
+		  if(i == '8' && j=='9')
+		         continue;
+			  putchar(',');
+                          putchar(' '); 
+	}
+}
+}
+ putchar('\n');
+	return (0);
+}
