144d5dee4c3c1233bd8a9fdf404c583f5f51c448
Your Name <you@example.com>
2022-06-02 23:15:04 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	
+	char l;
+      
+   for (l = 'a'; l <= 'z'; l++)
+        {
+                putchar(l);
+        }
+       
+  
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+		putchar(l);
+	}
+ 	putchar( '\n' );
+                	
+                    
+	return (0);
+}
