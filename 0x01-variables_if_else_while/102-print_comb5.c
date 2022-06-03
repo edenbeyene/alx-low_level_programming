2f3f23d48fbb889631fb4fdaad424da5e23c1120
Your Name <you@example.com>
2022-06-03 01:41:26 -0700

    Task 1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 int  i,j;
-for (i=0;i <= 99;i++)
+for ( i=0 ;i <= 99 ;i++)
 {
-	for(j= i+1;j <= 99;j++)
+	for( j= i+1;j <= 99 ; j++)
 	{
-		if(i!=j){
+		if( i != j){
 		 putchar((i / 10) + '0');
 		 putchar((i % 10) + '0');
                  
