4bb5a835de12cf188e1d3d73090355c0bb5de735
Your Name <you@example.com>
2022-06-07 14:11:15 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,15 +9,18 @@ void print_times_table(int n)
 {
 	int i, j;
 if (n == 0)
-{ 
+{
+
 _putchar(n + '0');
 _putchar('\n');
 
 }
 else if (n > 0 && n < 15)
-{	
+{
+
 for (i = 0; i <= n; i++)
 {
+
 _putchar('0');
 for (j = 1; j <= n; j++)
 putformat(i * j);
