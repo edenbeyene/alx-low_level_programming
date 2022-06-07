88dff9f0c313ecacd8f19c2b9add32d64b99ee76
Your Name <you@example.com>
2022-06-07 09:09:15 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,19 @@
 #include <unistd.h>
 #include "main.h"
+
 /**
- * _putchar - write a program prints _putchar 
- * @c: The character to print
- *
- * Return: On success 0.
-*/
-
-
-
-int main (void)
+ * main - Check description
+ * Description: It prints the word _putchar, followed by a new line.
+ * Return: 0.
+ */
+int main(void)
 {
-char word[8] = "_putchar";
-int i;
+	char word[8] = "_putchar";
+	int i;
 
-for (i = 0; i < 8; i++)
-{
-	_putchar(word[i]);
-        _putchar('\n');
-}
-        return (0);
-}
+	for (i = 0; i < 8; i++)
+		_putchar(word[i]);
+	_putchar('\n');
 
+	return (0);
+}
