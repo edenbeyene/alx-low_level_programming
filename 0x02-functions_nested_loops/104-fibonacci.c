c732d9270514c0f6e5d7cda4a2c908baf56c7827
Your Name <you@example.com>
2022-06-07 13:18:52 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,15 @@
 
 int main(void)
 {
-	int i, f1 = 1, f2 = 2, sum;
+	int i;
+long int f1 = 1, f2 = 2, sum;
 
-	printf("%d, %d, ", f1, f2);
+	printf("%dl, %dl, ", f1, f2);
 
 	for (i = 2; i < 98; i++)
 {
 		sum = f1 + f2;
-		printf("%d,", sum);
+		printf("%dl,", sum);
 
 		f1 = f2;
 		f2 = sum;
