3919b19ef73593c35cdcdd127dad46e5618f4009
Your Name <you@example.com>
2022-06-07 12:44:07 -0700

    task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 {
 	int i,f1 =1,f2 =2,sum;
 	
-	printf("%ld, %ld, ", f1, f2);
+	printf("%d, %d, ", f1, f2);
 
 	for (i = 2; i < 98; ++i)
 	{
 		sum = f1 + f2;
 		f1 = f2;
 		f2 = sum; 
-		printf("%ld\n", sum);
+		printf("%d\n", sum);
 	}
 
 	return (0);
