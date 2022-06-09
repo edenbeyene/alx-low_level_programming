76ba02303e1e9786aa25cde4a11455eaa12514a9
Your Name <you@example.com>
2022-06-09 05:43:10 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- *
- *
- *
- *
+ *print_most_numbers - Print numbers 0 to 9, excluding 2 and 4.
+ * Description: Can only use _putchar twice.
  */
-
 void print_most_numbers(void)
 {
 int i;
-for (i=0; i <= 9 ; i++)
+for (i = 0; i <= 9 ; i++)
 {
-if(i == 2 || i == 4)
+if (i == 2 || i == 4)
 {
 continue;
 }
