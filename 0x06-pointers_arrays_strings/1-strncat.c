d47222ee0e912a84699d3f28ba7292ad8849ab21
Your Name <you@example.com>
2022-06-15 03:17:41 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,28 @@
 #include "main.h"
+
+/**
+ *_strncat - function with two pointer char and one int
+ *@dest: character holds hello
+ *@src : character holds world!
+ *@n : integer valu the value copy
+ *Return: pointer to 'dest'
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i, j = 0;
+char *temp;
+temp = dest;
+for (i = 0; dest[i] != '\0'; i++)
+;
+while (n > 0)
+{
+dest[i] = src[j];
+i++;
+j++;
+n--;
+}
+dest[i] = '\0';
+return (dest);
+}
+
