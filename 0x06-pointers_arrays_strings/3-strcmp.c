ee79bc6cee97fc1360a183c9f4b5a9bb84df3a0e
Your Name <you@example.com>
2022-06-15 09:06:39 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i, j;
+int i, j, c = 0, r = 0, lim;
 int count1 = 0;
 int count2 = 0;
 for (i = 0; s1[i] != '\0'; i++)
@@ -20,14 +20,28 @@ for (j = 0 ; s2[j] != '\0'; j++)
 {
 count2++;
 }
-if (count1 > count2)
+if (count1 <= count2)
 {
-return (15);
+lim = count1;
 }
-else if (count1 < count2)
+else
 {
-return (-15);
+lim = count2;
 }
-return (0);
+while (c <= lim)
+{
+if (s1[c] == s2[c])
+{
+c++;
+continue;
+}
+else
+{
+r = s1[c] - s2[c];
+break;
+}
+c++;
+}
+return (r);
 }
 
