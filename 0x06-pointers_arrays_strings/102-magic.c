e3ac31aacae2ff3e921f60d45ecd6e0de1ae8a80
Your Name <you@example.com>
2022-06-15 11:39:59 -0700

    Task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,11 +9,11 @@ int *p;
 a[2] = 1024;
 p = &n;
 /**
-* main - Entry point
-* Description: 'the program's description'
-* @a: array of integer
-* @p: pointer
-* Return : Always 0 (Success)
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
 * - you are not allowed to code anything else than this line of code
 */
 a[2] = 98;
