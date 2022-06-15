506b05a152e43586988ca2135bb4cc8703b63dd4
Your Name <you@example.com>
2022-06-15 08:55:30 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,31 @@
 #include "main.h"
 
-
 /**
- * _strncpy - copy memory area
- * @dest: char array to copy into
- * @src: char array to copy from
- * @n: number of elements to copy
- * Return: pointer to `dest`
- */
+* _strncpy - copy memory area
+* @dest: char array to copy into
+* @src: char array to copy from
+* @n: number of elements to copy
+* Return: pointer to `dest`
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	char *temp;
-
-	temp = dest;
-	while (n > 0)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-		n--;
-	}
-
-	return (temp);
+int a = 0, b = 0;
+while (src[b])
+{
+b++;
 }
+while (n > a && src[a])
+{
+dest[a] = src[a];
+a++;
+}
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+
+return (dest);
+}
+
