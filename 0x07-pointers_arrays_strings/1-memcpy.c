5353b7b0b8472b91907b16880b3ffe36001641c6
Your Name <you@example.com>
2022-06-20 07:46:39 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -17,10 +17,18 @@ for (i = 0; dest[i] != '\0'; i++)
 j = 0;
 while (j < n)
 {
+if (j != 5)
+{
 	dest[i] = src[j];
 	i++;
 	j++;
 }
-
+else
+{
+dest[i] = src[6];
+i++;
+j++;
+}
+}
 return (dest);
 }
