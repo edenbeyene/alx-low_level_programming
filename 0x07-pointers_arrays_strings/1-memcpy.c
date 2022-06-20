a698f2160ffbcf5ade6f18c9708a2866c87641e8
Your Name <you@example.com>
2022-06-20 12:35:45 -0700

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,24 +11,9 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i, j;
-for (i = 0; dest[i] != '\0'; i++)
-;
-j = 0;
-while (j < n)
-{
-if (j != 5)
-{
-	dest[i] = src[j];
-	i++;
-	j++;
-}
-else
-{
-dest[i] = src[6];
-i++;
-j++;
-}
-}
-return (dest);
+unsigned int i;
+for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
 }
