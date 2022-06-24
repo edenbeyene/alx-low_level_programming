a197cb5cad7d4d2f4c1fa56629e4124c40e9d55d
Your Name <you@example.com>
2022-06-23 23:40:15 -0700

    Task 2

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *main - function return value
+ *@argc: array size argv
+ *@argv: the array of size argc
+ *
+ * Return: nothing
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
